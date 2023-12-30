3bd346db080034db220c5343eefbd83db16718c8
stan-donarise <111230475+stan-donarise@users.noreply.github.com>
2023-12-30 21:04:21 +1100

    '_' prefix for lib bundles

--- lib/three/three.web.ts
+++ lib/three/three.web.ts
@@ -1,5 +1,5 @@
 namespace $ {
 
-	export const $mpds_cifplayer_lib_three = require( '../mpds/cifplayer/lib/three/bundle/three.js' ) as typeof import( '../three/build/index' )
+	export const $mpds_cifplayer_lib_three = require( '../mpds/cifplayer/lib/three/_three.js' ) as typeof import( '../three/build/index' )
 
 }
